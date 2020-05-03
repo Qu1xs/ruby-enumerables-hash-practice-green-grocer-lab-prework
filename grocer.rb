7336620db9e79a659e0301337e9b02afe2181393
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-03 13:09:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,46 @@
+=begin
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+  i = 0
+  while i < cart.count do
+    cart_hash[i] = cart.map{ |x| [x, f(x)] }.to_h
+  puts cart_hash
+  return cart
+end
+=end
+def find_item(name, collection)
+  
+end
+
+def consolidate_cart(cart)
+        cons_cart = {}
+        index = 0
+
+        while index < cart.count do
+             
+            cons_cart[index] = find_item(cart[index][:item], cart)
+            cons_cart[index][:count] = 0
+            search_index = 0
+            
+            while search_index < cart.count do
+
+                    if cons_cart[index][:item] == cart[search_index][:item]
+                            cons_cart[index][:count] += 1 
+                    end
+                    search_index += 1
+            end
+            index += 1
+        end
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
 end
 
 def apply_clearance(cart)
-  # code here
+  
 end
 
 def checkout(cart, coupons)
-  # code here
+  
 end
